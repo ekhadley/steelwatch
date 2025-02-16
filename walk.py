be7693f14979fe9845f75933ce83d355f4081b61
Ethan Hadley <ekhadley@gmail.com>
2025-02-16 10:43:13 -0600

    asd

--- walk.py
+++ walk.py
@@ -1,15 +1,25 @@
 import RPi.GPIO as gpio
-import time
 
 gpio.setmode(gpio.BCM)
 
-inpin = 19
-outpin = 26
 
+inpin = 26
+outpin = 21
 if __name__ == "__main__":
-    gpio.setup(inpin, gpio.IN)
     gpio.setup(outpin, gpio.OUT)
+    gpio.setup(inpin, gpio.IN, pull_up_down=gpio.PUD_DOWN)
 
-    
+    while True:
+        inval = gpio.input(inpin)
+        print(inval)
 
     gpio.cleanup()
+
+"""
+ from metal side up on board-side connector, holes pointing forward:
+far right:  wheel forward
+1 from right: wheel backwards
+3 from right: constant 3.3v?
+4 from right: constant 3.3v?
+motor seems to work without the constant 3.3v lines.
+"""
